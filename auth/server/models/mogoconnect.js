1d552603916c9c7a7a77fb87b57b2878ec64706f
B-Mustansir <mustansirzain2@gmail.com>
2024-06-15 19:58:25 +0530

    remove server

--- auth/server/models/mogoconnect.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-const env=require('dotenv');
-env.config({ path: './config.env' });
-
-mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log("mongo DB connected succesfuly")}).catch((err)=>{console.log(err)});
-
-module.exports=mongoose;
